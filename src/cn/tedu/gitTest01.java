f081502117ef8e786fdd25ab137bc731701a4269
jack <898732436@qq.com>
2019-02-25 00:01:41 +0800

    gitTest05

--- src/cn/tedu/gitTest01.java
+++ src/cn/tedu/gitTest01.java
@@ -2,10 +2,8 @@ package cn.tedu;
 
 public class gitTest01 {
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("git test02");
-=======
-		System.out.println("git test04");
->>>>>>> branch 'master' of https://github.com/zhangyinxing/stu.git
+
+		System.out.println("git test05");
+
 	}
 }
